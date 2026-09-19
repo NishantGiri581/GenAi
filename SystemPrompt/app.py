d176c9a315a503765284a666e7bbbc1000c6b160
Nishant Giri <nishant581Giri@gmail.com>
2026-09-19 10:46:24 +0530

    Initial commit

---
+++ SystemPrompt/app.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash",
+)
+
+prompt = PromptTemplate.from_template(
+"""
+You are a safe, helpful, and concise AI assistant.
+Never hallucinate.
+"""
+)
+
+chain = prompt | llm
+
+while True:
+    user_input = input("Enter your question: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    response = chain.invoke({
+        "question": user_input
+    })
+
+    if isinstance(response.content, list):
+        text = "".join(
+            block["text"]
+            for block in response.content
+            if block.get("type") == "text"
+        )
+    else:
+        text = response.content
+
+    print("\nAI:", text)
